e3f5bdfcd9ff2fec22e3ca6e30580fc31eedbb8e
Kento210 <kentokentokento913@gmail.com>
2024-03-02 03:58:52 +0900

    add: 設定ファイルの追加

--- Assets/AI/ConersationSettings.cs
+++ Assets/AI/ConersationSettings.cs
@@ -1,18 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI; // UIコンポーネントを使用するため
+using TMPro; // TextMeshProを使用する場合
 
-public class ConersationSettings : MonoBehaviour
+public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputField userInputField; // ユーザー入力用のInputField
+    public TextMeshProUGUI messageText; // レスポンス表示用のTextMeshProUGUI
+
+    // ユーザーが入力を送信するためのメソッド
+    public void SubmitInput()
     {
-        
+        string userMessage = userInputField.text;
+        StartCoroutine(SendRequestToAPI(userMessage));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SendRequestToAPI(string message)
     {
-        
+        string apiUrl = "APIのURL"; // APIのエンドポイントURLを設定
+        WWWForm form = new WWWForm();
+        form.AddField("user_id", "1");
+        form.AddField("character_id", "1");
+        form.AddField("message", message);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(apiUrl, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("API Error: " + www.error);
+            }
+            else
+            {
+                // JSONからレスポンスメッセージをパースして表示
+                ResponseData responseData = JsonUtility.FromJson<ResponseData>(www.downloadHandler.text);
+                messageText.text = responseData.message;
+            }
+        }
+    }
+
+    [Serializable]
+    private class ResponseData
+    {
+        public string session_id;
+        public string message;
+        public Status status;
+    }
+
+    [Serializable]
+    private class Status
+    {
+        public string code;
+        public float execution_time;
     }
 }
